2824b12083fbb71f5c623a3856ec269020482838
Mathias Rav <rav@cs.au.dk>
2016-06-12 14:28:05 +0200

    Customize wsgi for prodekanus

--- j60adm/wsgi.py
+++ j60adm/wsgi.py
@@ -8,8 +8,20 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
 
-from django.core.wsgi import get_wsgi_application
+prev_sys_path = list(sys.path)
+site.addsitedir(
+    '/home/rav/j60adm/venv/lib/python3.4/site-packages')
+sys.path.append('/home/rav/j60adm')
+# reorder sys.path so new directories from the addsitedir show up first
+new_sys_path = [p for p in sys.path if p not in prev_sys_path]
+for item in new_sys_path:
+    sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+from django.core.wsgi import get_wsgi_application  # NOQA
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "j60adm.settings")
 
